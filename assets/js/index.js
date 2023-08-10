b8c0c86d9f3cfae162ab19b10af3e8514f6f2dea
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-08-10 08:19:35 +0200

    js auth controll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,63 @@
-const champ_username = register_form.elements["username"];
-const champ_email = register_form.elements["email"];
-const champ_password = register_form.elements["password"];
-const champ_confirm_password = register_form.elements["confirm_password"];
-
-const emailRegex = new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
-const usernameRegex = new RegExp("^[A-Za-z][A-Za-z0-9_]{7,29}$");
-const passwordRegex = new RegExp("/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/");
-
-champ_email.addEventListener("change", function(){
+window.addEventListener("DOMContentLoaded", function(){
+    const register_form = document.getElementById("register_form");
     
-    
-    if(champ_email.value.length === 0 || !emailRegex.exec(champ_email.value))
+    const champ_username = register_form.elements["username"]; 
+    const champ_email = register_form.elements["email"];
+    const champ_password = register_form.elements["password"];
+    const champ_confirm_password = register_form.elements["confirm_password"];
+
+    const error_username = document.getElementById("error_username");
+    const error_email = document.getElementById("error_email"); 
+    const error_password = document.getElementById("error_password"); 
+    const error_confirm_password = document.getElementById("error_confirm_password"); 
+
+    //* regex >>> au moins une lettre ou un chiffre
+    const usernameRegex = new RegExp("^[A-Za-z][A-Za-z0-9_]{0,49}$");
+    //* regex >>> au moins une lettre ou un chiffre au début, un arobase, une lettre ou un chiffre après l'arobase, un point, deux lettres ou deux chiffres à la fin
+    const emailRegex = new RegExp("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+    //* regex >>> au moins une majuscule, un chiffre, un caractère spécial, et 8 caractères
+    const passwordRegex = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+
+    checkRegex("username", champ_username, usernameRegex);
+    checkRegex("email", champ_email, emailRegex);
+    checkRegex("password", champ_password, passwordRegex);
+    checkRegex("confirm password", champ_confirm_password, passwordRegex);
+
+    champ_confirm_password.addEventListener("change", function(){
+        if(champ_confirm_password.value !== champ_password.value)
+        {
+            champ_confirm_password.classList.add("erreur");
+        }
+        else
+        {
+            champ_confirm_password.classList.remove("erreur");
+        }
+    })
+
+    champ_password.addEventListener("change", function(){
+        if(champ_confirm_password.value !== champ_password.value)
+        {
+            champ_confirm_password.classList.add("erreur");
+        }
+        else
+        {
+            champ_confirm_password.classList.remove("erreur");
+        }
+    })
+
+    function checkRegex(name, field, reg)
     {
-        champ_email.classList.add("erreur");
+        field.addEventListener("change", function(){
+
+            if(!reg.exec(field.value))
+            {
+                field.classList.add("erreur");
+                error_username.textContent = "faux";
+            }
+            else
+            {
+                field.classList.remove("erreur");
+            }
+        })
     }
-    else
-    {
-        champ_email.classList.remove("erreur");
-    }
-})
-
-champ_username.addEventListener("change", function(){
-    
-})
-
-champ_password.addEventListener("change", function(){
-    
-})
-
-champ_confirm_password.addEventListener("change", function(){
-    
 })
