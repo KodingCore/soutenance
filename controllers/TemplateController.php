974a8a17252da80118b7ca91f57fb1a2969f9a03
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-08-28 16:26:34 +0200

    css

--- controllers/TemplateController.php
+++ controllers/TemplateController.php
@@ -4,17 +4,22 @@ class TemplateController extends AbstractController
 {
 
     private TemplateManager $templateManager;
+    private CategoryManager $categoryManager;
+
 
     public function __construct()
     {
         $this->templateManager = new TemplateManager();
+        $this->categoryManager = new CategoryManager();
+
     }
    
     public function index()
     {
         $templates = $this->templateManager->getTemplates();
+        $categories = $this->categoryManager->getCategories();
 
-        $this->render("views/user/shop.phtml", ["templates" => $templates]);
+        $this->render("views/user/shop.phtml", ["templates" => $templates, "categories" => $categories]);
     }
     
 }
